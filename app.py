94edcb635d17f061206fd517de9666338612e3c9
Adam Jensen <jensen.ak@gmail.com>
2016-01-11 15:53:07 -0700

    Switch to eventlet

--- app.py
+++ app.py
@@ -1,37 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on available packages.
-async_mode = None
-
-if async_mode is None:
-    try:
-        import eventlet
-        async_mode = 'eventlet'
-    except ImportError:
-        pass
-
-    if async_mode is None:
-        try:
-            from gevent import monkey
-            async_mode = 'gevent'
-        except ImportError:
-            pass
-
-    if async_mode is None:
-        async_mode = 'threading'
-
-    print('async_mode is ' + async_mode)
-
 # monkey patching is necessary because this application uses a background
 # thread
-if async_mode == 'eventlet':
-    import eventlet
-    eventlet.monkey_patch()
-elif async_mode == 'gevent':
-    from gevent import monkey
-    monkey.patch_all()
+import eventlet
+async_mode = 'eventlet'
+eventlet.monkey_patch()
 
 import time
 from threading import Thread
