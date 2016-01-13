3f31e0febe392eaadf217e2f25f4243909eef44e
Adam Jensen <jensen.ak@gmail.com>
2016-01-12 21:39:24 -0700

    Starting game loop

--- game.py
+++ game.py
@@ -6,4 +6,22 @@ class Game():
         self.deck = deck
 
     def run(self):
-        
+        winner = False
+        while not winner:
+            self.deal()
+            self.play()
+            winner = self.gameStatus()
+
+    def deal(self):
+        #distribute opcodes
+        #start timer
+        #when all opcodes declared || timer expires, proceed
+        pass
+
+    def play(self):
+        # sort opcodes, perform in order with brief delay for interrupts
+        pass
+
+    def gameStatus(self):
+        # evaluate living players, players who have reached all goals?
+        return True
