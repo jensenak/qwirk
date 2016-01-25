d5e7c25a55a5e76e4f178e6e86723df0471b670c
Adam Jensen <jensen.ak@gmail.com>
2016-01-24 22:52:13 -0700

    Starting socket stuff

--- router.py
+++ router.py
@@ -10,6 +10,7 @@ async_mode = 'eventlet'
 eventlet.monkey_patch()
 
 import time
+import multiprocessing
 from threading import Thread
 from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
@@ -19,7 +20,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
+games = {}
 
+#Game bits
+# Import multiprocessing
+# have a dictionary of game rooms
+# start a process for each game
+# Message to send out on sockets?
 
 def background_thread():
     """Example of how to send server generated events to clients."""
@@ -42,11 +49,35 @@ def index():
     return render_template('index.html')
 
 
-@socketio.on('my event', namespace='/test')
-def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
-         {'data': message['data'], 'count': session['receive_count']})
+@socketio.on('join', namespace='/play')
+def join_game(message):
+    if not 'gameid' in message:
+        emit('fault', {'status': 'ERROR', 'message':"Game ID Required"})
+        return
+    gameid = message['gameid']
+    if not gameid in games:
+        games[gameid] = {"status":"lobby", "conns":[request.sid]}
+    else:
+        if games[gameid]['status'] != "lobby":
+            emit('fault', {'status': 'ERROR', 'message':"Game has already begun"})
+            return
+        games[gameid]['conns'].append(request.sid)
+    emit('ack',
+         {'status':'OK', 'message':'Joined game lobby'})
+
+@socketio.on('start', namespace='/play')
+def start_game(message):
+    if not 'gameid' in message:
+        emit('fault', {'status': 'ERROR', 'message':'Game ID Required'})
+        return
+    gameid = message['gameid']
+    if not gameid in games:
+        emit('fault', {'status': 'ERROR', 'message':'Game not found'})
+        return
+    if games[gameid]['status'] != 'lobby':
+        emit('fault', {'status': 'ERROR', 'message':'Game already running'})
+        return
+    multiprocessing.Process(target=games[gameid][game].run)
 
 
 @socketio.on('my broadcast event', namespace='/test')
