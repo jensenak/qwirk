31d0627ba34f3dd744dca9fc13f7b2682dba3e29
Adam Jensen <jensen.ak@gmail.com>
2016-02-01 22:07:51 -0700

    Finishing dealing/opcode selection

--- deck.py
+++ deck.py
@@ -1,5 +1,6 @@
 from queue import Queue
 from threading import Thread
+from time import sleep
 import json
 import random
 
@@ -19,30 +20,53 @@ class Deck():
                 self.deck.append(k)
         random.shuffle(self.deck)
 
-    def validateRegisters(self, player, opcodes):
+    def coerceRegisters(self, player, opcodes):
+        '''
+        We're enforcing 3 things:
+        1) Player has only changed unlocked cards
+        2) Player has only used cards dealt to him
+        3) Cards dealt have only been used once
+        :param player: player object
+        :param opcodes: list of opcodes to be assigned to player
+        :return: valid list of opcodes
+        '''
+        slicelen = min((game.maxDamage-player.damage), len(player.opcodes))
+        avail = player.opcodes[:slicelen] # copy unlocked opcodes
+        newops = []
+        for i in opcodes:
+            try:
+                # Remove the opcode from the available list and append it to the valid one
+                avail.remove(i)
+                newops.append(i)
+            except ValueError:
+                # Value error raised when opcode not in available list
+                newops.append(avail.pop) # Player shouldn't have cheated... now everything's messed up
+        return newops
 
     def deal(self):
-        for j in range(0, len(game.players)):
-            for j in range(0, min(self.handSize, game.maxDamage - game.players[i]['damage'])):
-                #As player sustains more and more damage, opcodes get locked
-                op = self.deck.pop()
-                game.players[j]['opcodes'][j] = op
-
         async = {}
         q = Queue(maxsize=0)
-        for j in range(0, len(self.players)):
-            t = Thread(target=self.io.receive, args=(q, self.players[j]))
-            async[self.players[j]['name']] = {"q":q, "t":t}
+        for j in range(0, len(game.players)):
+            for j in range(0, min(self.handSize, game.maxDamage - game.players[i].damage)):
+                #As player sustains more and more damage, opcodes get locked
+                op = self.deck.pop()
+                game.players[j].opcodes[j] = op
+            #After assigning cards, start a thread to receive input from player
+            t = Thread(target=game.io.receive, args=(q, game.players[j], 300))
+            async[game.players[j].name] = {"q":q, "t":t}
             t.start()
 
-        recvd = []
+        expire = Thread(target=qTimer, args=(q, 90))
+        expire.start()
         while True:
             resp = q.get(block=True)
             if resp['src'] == "qtimer":
-                break
-            resp['src']['opcodes'] = self.deck.validate(resp['src'], resp['data'])
+                break #Out of time, all unrecv'd players will retain card order as dealt
+            resp['src'].opcodes = self.validate(resp['src'], resp['data'])
             recvd.append(resp['src'])
+        # Note that dealing puts opcodes in a player's opcode list. If they didn't respond
+        # during the window above, they'll just retain the opcodes as dealt
 
-        for j in range(0, len(self.players)):
-            if self.players[j] not in recvd:
-                self.players[j]['opcodes'] = self.deck.validate(self.players[j], "")
+    def qTimer(self, q, timeout):
+        sleep(timeout)
+        q.put({"src":"qtimer", "data":"done"})
--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 from threading import Thread
-from queue import Queue
 from time import sleep
+import re
 import field, player, deck
 
 #GLOBAL GAME OBJECT
@@ -13,15 +13,13 @@ class Game():
         self.deck = deck
         self.io = ioModule
         self.maxDamage = 8
-x
+
     def run(self):
         winner = False
         while not winner:
             self.deal()
             self.play()
             winner = self.gameStatus()
-
-
 
     def play(self):
         # sort opcodes, perform in order with brief delay for interrupts
@@ -31,10 +29,6 @@ x
         # evaluate living players, players who have reached all goals?
         return True
 
-    def qTimer(self, q, timeout):
-        sleep(timeout)
-        q.put({"src":"qtimer", "data":"done"})
-
 class IOModule():
     def notify(self, player, msg):
         print("NOTIFY: {}".format(msg))
@@ -42,10 +36,22 @@ class IOModule():
     def broadcast(self, msg):
         print("BROADCAST: {}".format(msg))
 
-    def receive(self, q, player):
-        for i in range(0, 15):
-            sleep(1)
-        q.put({"src":player, "data":"done"})
+    def receive(self, q, player, timeout):
+        expire = Thread(target=sleep, args=(timeout,))
+        expire.start()
+        while expire.isAlive:
+            print("Player {}".format(player.name))
+            print([op.name for op in player.opcodes])
+            nums = input('Type comma separated numbers for desired card order: ')
+        choices = map(int, re.findall('\d', nums))
+        final = []
+        try:
+            for n in choices:
+                final.append(player.opcodes[n])
+        except Exception:
+            final = player.opcodes # Take that ya fart face... throwing exceptions... how rude
+
+        q.put({"src":player, "data":final})
         return
 
 if __name__ == "__main__":
